9caeb954caaa9f8a0055db5d78c7e51a3f0cc475
gorkalaucirica <gorka.lauzirika@gmail.com>
2016-05-10 17:02:03 +0200

    Added arrow to panel header to toggle open/close

--- src/LIN3S/AdminBundle/Resources/public/js/panel.js
+++ src/LIN3S/AdminBundle/Resources/public/js/panel.js
@@ -14,6 +14,6 @@
 
 (function ($) {
   $('.panel__header').click(function() {
-    $(this).parent().find('.panel__content').toggleClass('panel__content--hidden');
+    $(this).parent().toggleClass('panel--closed');
   });
 }(jQuery));

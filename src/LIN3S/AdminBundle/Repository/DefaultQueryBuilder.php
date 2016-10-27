bdbbfe864fd03bd730544e7ce07e61a8c04ccb0a
benatespina <benatespina@gmail.com>
2016-10-27 09:48:48 +0200

    Become the default query builder more flexible

--- src/LIN3S/AdminBundle/Repository/DefaultQueryBuilder.php
+++ src/LIN3S/AdminBundle/Repository/DefaultQueryBuilder.php
@@ -13,6 +13,7 @@ namespace LIN3S\AdminBundle\Repository;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\QueryBuilder as DoctrineQueryBuilder;
 use LIN3S\AdminBundle\Configuration\EntityConfiguration;
 use LIN3S\AdminBundle\ListFields\ListField;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +23,7 @@ use Symfony\Component\HttpFoundation\Request;
  *
  * @author Gorka Laucirica <gorka.lauzirika@gmail.com>
  * @author Jagoba Perez <jagoba@lin3s.com>
+ * @author Beñat Espiña <benatespina@gmail.com>
  */
 class DefaultQueryBuilder implements QueryBuilder
 {
@@ -57,60 +59,40 @@ class DefaultQueryBuilder implements QueryBuilder
             $queryBuilder->join('a.' . $association, 'join_' . $association);
         }
 
-        if ($request->get('orderBy')) {
-            $possibleAssociation = explode('.', $request->get('orderBy'))[0];
+        list($alias, $fields) = $this->getQueryAliasAndFields(
+            $request->get('orderBy'),
+            $request->get('order'),
+            $metadata,
+            $queryBuilder
+        );
 
-            $found = false;
-            foreach ($metadata->getAssociationMappings() as $associationMapping) {
-                if ($possibleAssociation === $associationMapping['fieldName']) {
-                    $queryBuilder->addOrderBy(
-                        'join_' . $associationMapping['fieldName'] . '.' . explode('.', $request->get('orderBy'))[1],
-                        $request->get('order', 'ASC')
-                    );
-                    $found = true;
-                    continue;
-                }
-            }
-            if (!$found) {
-                $queryBuilder->addOrderBy('a.' . $request->get('orderBy'), $request->get('order', 'ASC'));
-            }
+        if ($alias) {
+            null === $fields
+                ? $queryBuilder->addOrderBy($alias . $request->get('orderBy'), $request->get('order', 'ASC'))
+                : $queryBuilder->addOrderBy($alias . '.' . $fields, $request->get('order', 'ASC'));
         }
 
-        if ($request->get('filterBy') && $request->get('filter')) {
-            $previousId = 97;
 
-            if ($this->isTableRelation($metadata, $request->get('filterBy'))) {
-                $associations = explode('.', $request->get('filterBy'));
-                $fields = $associations;
-                for ($i = 0; $i < count($associations) - 1; ++$i) {
-                    if (false === $this->isTableRelation($metadata, $associations[$i] . '.' . $associations[$i + 1])) {
-                        break;
-                    }
-                    $queryBuilder->innerJoin(chr($previousId) . '.' . $associations[$i], chr($previousId + 1));
-                    ++$previousId;
-                    foreach ($metadata->getAssociationMappings() as $associationMapping) {
-                        if ($associationMapping['fieldName'] === $associations[$i]) {
-                            $metadata = $this->manager->getClassMetadata($associationMapping['targetEntity']);
-                        }
-                    }
-                    unset($fields[$i]);
-                }
+        list($alias, $fields) = $this->getQueryAliasAndFields(
+            $request->get('filterBy'),
+            $request->get('filter'),
+            $metadata,
+            $queryBuilder
+        );
 
-                $fields = implode('.', $fields);
-
-                $queryBuilder->where($queryBuilder->expr()->like(
-                    chr($previousId) . '.' . $fields,
-                    "'%" . $request->get('filter') . "%'"
-                ));
-            } else {
-                $queryBuilder->where($queryBuilder->expr()->like(
-                    chr($previousId) . '.' . $request->get('filterBy'),
-                    "'%" . $request->get('filter') . "%'"
-                ));
-            }
+        if ($alias) {
+            null === $fields
+                ? $queryBuilder->where($queryBuilder->expr()->like(
+                $alias . '.' . $request->get('filterBy'),
+                "'%" . $request->get('filter') . "%'"
+            ))
+                : $queryBuilder->where($queryBuilder->expr()->like(
+                $alias . '.' . $fields,
+                "'%" . $request->get('filter') . "%'"
+            ));
         }
 
-        return $queryBuilder;
+        return $this->removeDuplicateJoins($queryBuilder);
     }
 
     /**
@@ -156,5 +138,82 @@ class DefaultQueryBuilder implements QueryBuilder
         }
 
         return $associations;
+    }
+
+    /**
+     * Gets the result query alias and related fields.
+     *
+     * @param string               $criteriaBy   The criteria key
+     * @param string               $criteria     The criteria value
+     * @param ClassMetadata        $metadata     The Doctrine class metadata
+     * @param DoctrineQueryBuilder $queryBuilder The query builder
+     *
+     * @return array
+     */
+    private function getQueryAliasAndFields(
+        $criteriaBy,
+        $criteria,
+        ClassMetadata $metadata,
+        DoctrineQueryBuilder $queryBuilder
+    ) {
+        if ($criteriaBy && $criteria) {
+            $previousId = 97;
+
+            if ($this->isTableRelation($metadata, $criteriaBy)) {
+                $associations = explode('.', $criteriaBy);
+                $fields = $associations;
+                for ($i = 0; $i < count($associations) - 1; ++$i) {
+                    if (false === $this->isTableRelation($metadata, $associations[$i] . '.' . $associations[$i + 1])) {
+                        break;
+                    }
+                    $queryBuilder->innerJoin(chr($previousId) . '.' . $associations[$i], chr($previousId + 1));
+                    ++$previousId;
+                    foreach ($metadata->getAssociationMappings() as $associationMapping) {
+                        if ($associationMapping['fieldName'] === $associations[$i]) {
+                            $metadata = $this->manager->getClassMetadata($associationMapping['targetEntity']);
+                        }
+                    }
+                    unset($fields[$i]);
+                }
+                $fields = implode('.', $fields);
+
+                return [
+                    chr($previousId), $fields,
+                ];
+            }
+
+            return [
+                chr($previousId),
+            ];
+        }
+    }
+
+    /**
+     * Removes the duplicate joins of given query builder.
+     *
+     * @param DoctrineQueryBuilder $queryBuilder The query builder
+     *
+     * @return DoctrineQueryBuilder
+     */
+    private function removeDuplicateJoins(DoctrineQueryBuilder $queryBuilder)
+    {
+        $joinArray = [];
+        $joins = $queryBuilder->getDQLParts()['join']['a'];
+        foreach ($joins as $join) {
+            $joinArray[] = $join->getAlias();
+        }
+        $joinArray = array_unique($joinArray);
+        foreach ($joins as $key => $join) {
+            if (!array_key_exists($key, $joinArray)) {
+                unset($joins[$key]);
+            }
+        }
+
+        $queryBuilder->resetDQLPart('join');
+        foreach ($joins as $join) {
+            $queryBuilder->join($join->getJoin(), $join->getAlias());
+        }
+
+        return $queryBuilder;
     }
 }

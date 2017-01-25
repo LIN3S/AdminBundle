591511c14bd54b5912ce3b87b0bc767ee0fbf104
gorkalaucirica <gorka.lauzirika@gmail.com>
2017-01-25 11:56:19 +0100

    Added test application

---
+++ tests/Application/app/AppKernel.php
@@ -0,0 +1,42 @@
+<?php
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+final class AppKernel extends \Symfony\Component\HttpKernel\Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \LIN3S\AdminBundle\LIN3SAdminBundle()
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config.yml');
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+}
